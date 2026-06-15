289013190ee9837546188d23fa5e7de1a7f963a2
ARod03 <ardgz2003@gmail.com>
2026-06-15 15:41:11 -0500

    Added a constructor + getters/setters to Product.java

--- src/main/java/com/pluralsight/northwind_traders_springboot/model/Product.java
+++ src/main/java/com/pluralsight/northwind_traders_springboot/model/Product.java
@@ -7,10 +7,48 @@ import jakarta.persistence.*;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int productID;
+    private Long productID;
     private String name;
     private String category;
     private double price;
 
+    public Product(Long productID, String name, String category, double price) {
+        this.productID = productID;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Long productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
